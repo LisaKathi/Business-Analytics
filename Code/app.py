397dd899d0f800049623519bfccfcb92d263d6ef
Leo di Luzio <lucaluzio5@gmail.com>
2022-12-18 20:52:08 +0100

    dritter plot, übersetzung ins deutsche, layout fine-tuning und code commenting

--- Code/app.py
+++ Code/app.py
@@ -5,7 +5,6 @@
 ##### 
 
 import streamlit as st
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,9 +12,9 @@ import pickle
 
 st.set_page_config(
     page_title="Online Shopper Revenue App",
-    layout="wide"
-    )
+    layout="wide")
 
+############# Funtionen, Modelle und Daten laden ####################
 
 # import dataset and cache it (so it has not to be loaded every time)
 @st.cache()
@@ -23,6 +22,12 @@ def load_data():
     data = pd.read_csv("../Data/online_shoppers_app_dev.csv")
     return(data.dropna())
 data = load_data()
+
+@st.cache()
+def load_variable_explanation():
+    data = pd.read_excel("../Data/Variable_Explanation.xlsx")
+    return(data.dropna())
+variable_explanation = load_variable_explanation()
 
 @st.cache(hash_funcs={'xgboost.sklearn.XGBClassifier': id})
 def load_model():
@@ -36,26 +41,42 @@ def add_space(lines):
     for i in range(lines):
         st.write("\n")
 
+################### Einleitung #########################
 
 st.title("Online Shopper Revenue Predictor App")
 
-st.write("This App predicts whether a person browsing online on shopping pages will eventually purchase something at the end of the session")
+st.header("Einführung: Die Revenue-Predictor App")
+
+st.write("Kaufen oder nicht kaufen? Diese Frage stellt sich nicht nur für Besucher von Online Shopping Portalen, sondern auch die Betreiber \
+         der Webseiten. Zum einem ist Kenntnis über Kunden ein großer Vorteil für effektives Marketing. Zum anderen hilft es realistische \
+        Budgets zu erstellen, wenn Einnahmen verlässlich vorhergesagt werden können. Wissen über Kaufentscheidungen ist daher eine wertvolle \
+        Ressource für zahlreiche Unternehmen. Um diese Fragen zu beantworten sagt diese App für individuelle Kunden voraus, ob es am Ende eines\
+        Website-Besuchs zu einer Transaktion kommt oder nicht.")
+st.write("Für die Vorhersage wird ein XGBoost-Model verwendet, welches schnell und zuverlässig eine Vorhersage trifft.\
+         Die ausschlagebensten Variablen sind PageValue, Month und ReturingVisitor.")
+
+if st.checkbox("Klicke hier, um die Erklärung der Variablen anzuzeigen"):
+    st.write(variable_explanation)
 
 st.header("Modell-Informationen")
 
 add_space(3)
 
+
+#################### User Input #########################
+
+
 # Introducing three colums for user inputs
 row1_col1, row1_col2, row1_col3 = st.columns([1,1,1])
 
-p_value = row1_col1.slider("PageValue",
+# slider page values
+p_value = row1_col1.slider("Page-Value",
                   data["PageValues"].min(),
                   data["PageValues"].max(),
                   (0.0, data["PageValues"].max()))
 
-
+#input für Monat
 monatsnamen = {"Februar": 2, "März": 3, "Mai": 5, "Juni": 6, "Juli": 7, "August": 8, "September": 9, "Oktober": 10, "November": 11, "Dezember": 12}
-
 month = row1_col2.multiselect("Monat der Session", monatsnamen.keys())
 
 if month:
@@ -63,9 +84,9 @@ if month:
 else:
     Monat = [2, 3, 5, 6, 7, 8, 9, 10, 11, 12]
 
+names = data.drop(columns="Revenue").columns
 
-
-names = data.columns
+# input individuelle variablen
 variable = row1_col3.selectbox("Auswahl der Variable", names)
 
 # creating filtered data set according to slider inputs
@@ -76,11 +97,13 @@ filtered_data = data.loc[(data["PageValues"] >= p_value[0]) &
 
 add_space(3)
 
+############################### Plots #################################
 
-# defining two columns for layouting plots 
+# defining three columns for plots 
 
-row2_col1, row2_col2  = st. columns([1,1])
+row2_col1, row2_col2, row2_col3  = st. columns([0.7,1,1])
 
+# Erster Plot 
 row2_col1.subheader("Revenue")
 
 if filtered_data.empty:
@@ -117,13 +140,30 @@ else:
 
 # Zweiter Plot
 
-
 fig2 = sns.displot(x=data[variable], hue=data["Revenue"], kind="kde", palette="Set2", multiple="stack")
-row2_col2.subheader("Dichteverteilung der Variable")
+row2_col2.subheader("Dichteverteilung der Variable *{}*".format(variable))
 row2_col2.pyplot(fig2)
 
-add_space(5)
 
+
+data_proba = data.drop(columns="Revenue").copy()
+probability = pd.DataFrame(model.predict_proba(data_proba))
+data_proba["Revenue Probability"] = probability.iloc[:,1]
+#data_proba[["Probability 0"],["Probability"]] = model.predict_proba(data_proba)
+
+# Dritter Plot
+row2_col3.subheader("Wahrscheinlichkeit Revenue in Abhängigkeit der Variable *{}*".format(variable))
+
+fig3, ax = plt.subplots(figsize=(10,7.5))
+ax.scatter(data_proba[variable], data_proba["Revenue Probability"], edgecolor='#4d4d4d', label=variable, alpha=0.8)
+ax.set_xlabel(variable, fontsize=15)
+ax.set_ylabel("Wahrscheinlichkeit einer Transaktion in Prozent", fontsize=15)
+ax.grid()
+ax.legend().set_title("Variable")
+ax.set_facecolor("#f5f5fa")
+row2_col3.pyplot(fig3)
+
+add_space(5)
 
 ############################# Guessing Game #################################
 
@@ -146,15 +186,14 @@ row3_col1, row3_col2 = st.columns([1,1])
 st.markdown("***")
 
 row3_col1.subheader("Guessing Game")
-row3_col1.write("Select a person and predict based on the given\
-                parameters in the table on the right, whether \
-                this online shopper will generate a revenue or not\
-                meaning if a purchase happens or not")
+row3_col1.write("Wähle eine Person aus und entscheide, basierend auf den drei wichtigsten Werten \
+                in der Tabelle auf der rechten Seite, ob eine Transaktion stattfindet oder nicht")
 with row3_col1.form(key="sample_form"):
-    sample = st.selectbox("Select a person:", 
+    sample = st.selectbox("Wähle eine Person aus:", 
                           test_samples.index)
-    guess = st.radio("Make a prediction based on the values for that person as to whether that person will purchase something online (Revenue) or not (No Revenue)",
-                     ('Revenue', 'No Revenue'))
+    guess = st.radio("Entscheide, ob die ausgewöhlte Person nach dem besuch der Shopping-Website \
+                     eine Transaktion durchführt oder nicht",
+                     ('Transaktion', 'Keine Transaktion'))
     submit = st.form_submit_button("Submit")
     if submit:
         sample_rev = test_frac.loc[test_frac.index == sample, "Revenue"].item()
@@ -162,33 +201,32 @@ with row3_col1.form(key="sample_form"):
         #st.write(test_samples.loc[test_samples.index == sample,])
         
         
-        if ((sample_rev == 1) and (guess == "Revenue")) or((sample_rev == 0) and (guess == "No Revenue")):
-            st.write("Congratulations, your prediction is **correct**")
-        elif ~((sample_rev == 0) and (guess == "No Revenue")) or ~((sample_rev == 1) and (guess == "Revenue")):
-            st.write("Unfortunately your prediction is **wrong**")
+        if ((sample_rev == 1) and (guess == "Transaktion")) or((sample_rev == 0) and (guess == "Keine Transaktion")):
+            st.write("Gratulation, deine Vorhersage ist **korrekt**")
+        elif ~((sample_rev == 0) and (guess == "Keine Transaktion")) or ~((sample_rev == 1) and (guess == "Transaktion")):
+            st.write("Leider ist deine Vorhersage **falsch**")
         if sample_rev == 1:
-            st.write("In this case the online shopper **does** generate a revenue")
+            st.write("In diesem Fall findet **eine** Transaktion statt")
         if sample_rev == 0:
-            st.write("In this case the online shopper **does not** generate a revenue")
-        if ((sample_pred == 0) and (guess == "No Revenue")) or ((sample_pred == 1) and (guess == "Revenue")):
-            st.write("Your Predictions is **congruent** with the prediction of the model")
-        elif ~((sample_pred == 0) and (guess == "No Revenue")) or ~((sample_pred == 1) and (guess == "Revenue")):
-            st.write("Your prediction is **different** from the models' prediction")
+            st.write("In diesem Fall findet **keine** Transaktion statt")
+        if ((sample_pred == 0) and (guess == "Keine Transaktion")) or ((sample_pred == 1) and (guess == "Transaktion")):
+            st.write("Die App hat **dieselbe** Vorhersage wie Du getroffen")
+        elif ~((sample_pred == 0) and (guess == "Keine Transaktion")) or ~((sample_pred == 1) and (guess == "Transaktion")):
+            st.write("Die App hat eine **andere** Vorhersage als Du getroffen")
         else: 
-            st.write("Please check your input again")
+            st.write("Bitte überprüfe deine Eingabe nocheinmal")
 
 ### Display the table with the values for the guessing game        
 
-if row3_col2.checkbox("Press here to see the Values for each person"):
+if row3_col2.checkbox("Klicke hier um die Werte für jede Person zu sehen"):
 
-    row3_col2.write("This table shows for each person three of the most relevant values\
-                    determining whether an online shopper will generate\
-                    a revenue")
+    row3_col2.write("Diese Tabelle zeigt für jede Person die drei wichtigsten Werte für die Vorhersage der \
+                    Zielvariable *Revenue*, also ob eine Transaktion stattfindet oder nicht")
     table_samples = test_samples.copy()
-    table_samples.rename(columns={"PageValues": "Page Value in US-Dollar",
-                                  "VisitorType_Returning_Visitor": "Returning Visitor"}, inplace=True)
-    table_samples["Month"].replace({2:'Feburary',3:"March",5:"May",6:"June",7:"July",8:"August",9:"September",10:"October",11:"November",12:"December"}, inplace=True)
-    table_samples["Returning Visitor"].replace({1:"Yes",0:"No"}, inplace=True)
+    table_samples.rename(columns={"PageValues": "Page-Value in US-Dollar", "Month":"Monat",
+                                  "VisitorType_Returning_Visitor": "Wiederkehrender Kunde"}, inplace=True)
+    table_samples["Monat"].replace({2:'Februar',3:"März",5:"Mai",6:"Juni",7:"Juli",8:"August",9:"September",10:"Oktober",11:"November",12:"Dezember"}, inplace=True)
+    table_samples["Wiederkehrender Kunde"].replace({1:"Ja",0:"Nein"}, inplace=True)
     table_samples.index = table_samples.index.rename("Person")
     
     row3_col2.write(table_samples.to_html(), unsafe_allow_html=True)
@@ -197,19 +235,21 @@ if row3_col2.checkbox("Press here to see the Values for each person"):
 add_space(5)
 
 # predict revenue for uploaded data
-st.header("Predicting if customer purchases something or not")
+st.header("Vorhersage ob eine Transaktion zustande kommt oder nicht")
 
-uploaded_data = st.file_uploader("Choose a file with Customer Data for making predictions")
+uploaded_data = st.file_uploader("Wähle eine csv-Datei mit Kundendaten aus, um eine Vorhersage zu treffen")
 
 # only make predictions if data is uploaded
 if uploaded_data is not None:
     new_customers = pd.read_csv(uploaded_data)
     new_customers = pd.get_dummies(new_customers, drop_first=True)
+
+if st.checkbox("Klicke hier, wenn Du die vorhergesagten Daten sehen willst"):
     st.write(new_customers)
     
     new_customers["predictions"] = model.predict(new_customers)
     
-    st.download_button(label="Download Scored Customer Data",
+    st.download_button(label="Download vorhergesagte Kunden-Daten",
                        data=new_customers.to_csv().encode("utf-8"),
                        file_name="scored_new_customers.csv")
     
